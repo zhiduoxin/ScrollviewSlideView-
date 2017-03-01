5413ab0447982d5e054ee4f22be117433682411d
lx <lx@lxdemacbook-pro.local>
2017-03-01 09:43:13 +0800

    Initial Commit

---
+++ ScrollviewSlideView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollviewSlideView
+//
+//  Created by lx on 17/3/1.
+//  Copyright © 2017年 lx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollviewSlideView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollviewSlideView
+//
+//  Created by lx on 17/3/1.
+//  Copyright © 2017年 lx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
